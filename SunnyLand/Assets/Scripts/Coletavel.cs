d92f3fcf755bf2491f0bb274089cb52e51cadaa9
Duda <mariaeduardadepaulasantos416@gmail.com>
2025-06-18 09:22:18 -0300

    Colisão e efeitos - aula 2

--- SunnyLand/Assets/Scripts/Coletavel.cs
+++ SunnyLand/Assets/Scripts/Coletavel.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Coletavel : MonoBehaviour
 {
     private int pontos = 5;
-
+    public TMP_Text texto_pontos;
+    public ParticleSystem efeito;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,16 @@ public class Coletavel : MonoBehaviour
     void Update()
     {
         //Debug.Log("Rodando");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) ;
+        {
+            pontos++;
+            texto_pontos.text = "Pontos: " + pontos.ToString();
+            collision.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            Instantiate(efeito, transform.position, Quaternion.identity);
+        }
     }
 }
--- SunnyLand/Assets/Scripts/Personagem.cs
+++ SunnyLand/Assets/Scripts/Personagem.cs
@@ -9,14 +9,14 @@ public class personagem : MonoBehaviour
     private bool isDead = false;
     public float velocidade = 1.2f;
     private Rigidbody2D rb;
-    //public float novaGravidade = 0;
+    public float novaGravidade = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Olá eu sou\n" + nome + "Vamos iniciar com\n" + vida + " vidas");
-        rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 9.8f;
+        //rb = GetComponent<Rigidbody2D>();
+       // rb.gravityScale = 9.8f;
       //rb.gravityScale = novaGravidade;
     }
 
